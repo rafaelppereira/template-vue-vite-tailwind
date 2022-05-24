18a1f7a92a750a5a28c08410b199d4c40644f9a4
Rafael <rafaelsantospereira03@gmail.com>
2022-05-24 14:20:51 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div className="bg-gray-900 h-screen flex justify-center items-center flex-col"> 
+      <h1 className="text-3xl font-bold text-white">Hello Dev 👋</h1>
+      <p className="max-w-md text-center mt-6 text-gray-400 font-normal">
+        This template was created to facilitate the development process.
+        <a href="https://github.com/rafaelppereira" target="_blank" className="text-green-500 underline inline-flex ml-1">Created by Rafael Pereira</a>
+      </p>
+      <span className="text-cyan-500 mt-4">Vue + Vite + TailwindCSS</span>
+    </div>
+</template>
